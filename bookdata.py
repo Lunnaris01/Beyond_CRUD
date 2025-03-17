17cfaff61ac2566695c43bc27ee2dc8da5e9bcce
Lunnaris01 <Julian_Ullrich@live.de>
2025-03-17 16:59:20 +0100

    part2 using in memory data

---
+++ bookdata.py
@@ -0,0 +1,23 @@
+books = [
+	{
+		"id": 1,
+		"title": "Title1",
+		"author": "Author1",
+		"publisher": "Publisher1",
+		"published_date": "2021-01-01",
+		"page_count": 1234,
+		"language": "english",
+	},
+	{
+		"id": 2,
+		"title": "Title2",
+		"author": "Author2",
+		"publisher": "Publisher2",
+		"published_date": "2021-01-05",
+		"page_count": 4321,
+		"language": "german",
+	}
+]
+
+
+
--- main.py
+++ main.py
@@ -1,43 +1,51 @@
-from fastapi import FastAPI, Header
-from typing import Optional
+from fastapi import FastAPI, Header, status
+from fastapi.exceptions import HTTPException
+from typing import Optional, List
 from pydantic import BaseModel
+from bookdata import books
 
 app = FastAPI()
 
+class Book(BaseModel):
+		id: int
+		title: str
+		author: str
+		publisher: str
+		published_date: str
+		page_count: int
+		language: str
 
 
-@app.get('/')
-async def read_root():
-	return {"message": "Hello World"}
 
-@app.get("/greet")
-async def greet_user(name:Optional[str] = "User") -> dict:
-	return {"message": f"Hello {name}"}
+@app.get('/books', response_model = List[Book])
+async def get_all_books():
+	return books
 
-@app.get("/greet/{name}")
-async def greet_user_with_query(name:str) -> dict:
-	return {"message": f"Hello {name}"}
+@app.get('/books/{book_id}')
+async def get_book(book_id:int) -> dict:
+	for book in books:
+		if book["id"] == book_id:
+			return book
+	raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
-class BookCreateModel(BaseModel):
-	title : str	
-	author : str
+@app.post('/books', status_code = status.HTTP_201_CREATED)
+async def add_book(book_data:Book)->dict:
+	bookentry = book_data.model_dump()
+	books.append(bookentry)
+	return bookentry
 
+@app.patch('/books/{book_id}')
+async def update_book(book_id:int, book_data:Book) -> dict:
+	for i, book in enumerate(books):
+		if book["id"] == book_id:
+			books[i] = book_data.model_dump()
+			return {}
+	raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
-@app.post("/books")
-async def create_book(book_data:BookCreateModel):
-	return {
-		"title": book_data.title,
-		"author": book_data.author
-	}
-
-@app.get('/get_headers', status_code = 200)
-async def get_headers(
-	accept:str = Header(None),
-	content_type: str = Header(None),
-	content_length: str = Header(None)
-):
-	request_headers = {}
-	request_headers["Accept"] = accept
-	request_headers["Content-Type"] = content_type
-	request_headers["Content-Length"] = content_length
-	return request_headers
+@app.delete("/books/{book_id}")
+async def delete_book(book_id:int):
+	for i, book in enumerate(books):
+		if book["id"] == book_id:
+			books.remove(i)
+			return {}
+	raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
