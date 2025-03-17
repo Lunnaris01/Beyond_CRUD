451170067c6abd4bb9fd0443aae29e82cb15ce0d
Lunnaris01 <Julian_Ullrich@live.de>
2025-03-18 00:49:38 +0100

    basic postgresql setup

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
+from src.db.main import init_db
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def life_span(app:FastAPI):
+    print("Starting Server...")
+    await init_db()
+    yield
+    print("Server has been stopped")
+
 
 version = "v0.0.1"
 
 app = FastAPI(
     title = "Bookly",
     description="A REST API for books",
-	version=version
+	version=version,
+	lifespan=life_span
 )
+
 app.include_router(book_router, prefix=f"/api/{version}/books")
